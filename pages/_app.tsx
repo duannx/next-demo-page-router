7c919c3acb439a9b040489bca8be5b4c83c2b03c
duandz <xuanduannguyen@gmail.com>
2024-11-12 15:05:08 +0700

    add routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log('app running')
-  return <Component {...pageProps} />;
+  console.log("app running");
+  return (
+    <>
+      <ProgressBar
+        height="4px"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/"> Home </Link>
+            </li>
+            <li>
+              <Link href="/blog"> Blog </Link>
+            </li>
+            <li>
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+              <a href="/"> Home but SSR </a>
+            </li>
+          </ul>
+        </nav>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
