afc3399d0b6f6574e5bdf31c982c464c49305fc5
duandz <xuanduannguyen@gmail.com>
2024-11-12 14:55:09 +0700

    add routes

---
+++ pages/blog/2024/[...slug].tsx
@@ -0,0 +1,3 @@
+export default function Blog()  {
+    return <h1>This is catch all blog/2024/[...slug].tsx</h1>
+}
---
+++ pages/blog/[[...slug]].tsx
@@ -0,0 +1,3 @@
+export default function Blog()  {
+    return <h1>This is OPTIONAL catch all blog/[[...slug]].tsx</h1>
+}
